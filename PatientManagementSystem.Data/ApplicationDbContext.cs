a5bda4445b42822bf41a4b82ca1c7a39cc10fa14
User <greenzeb@inbox.lv>
2023-06-17 18:59:04 +0300

    Add project files.

---
+++ PatientManagementSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatientManagementSystem;
+using System.Collections.Generic;
+
+namespace PatientManagementSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Define DbSet for the Patient entity
+        public DbSet<Patient> Patients { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Add other DbSet properties for your entities as needed
+
+        // Override OnModelCreating method if you need to configure entity relationships or apply additional configurations
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     // Configure entity relationships and other configurations
+        // }
+    }
+}
---
+++ PatientManagementSystem/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using PatientManagementSystem;
+using PatientManagementSystem.Data;
+
+namespace PatientManagementSystem
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("FlightPlannerConnection")));
+
+            services.AddScoped<ApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+
+            services.AddControllersWithViews();
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors("CorsPolicy");
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
